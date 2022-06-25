6bbba370297ba34fb5a1ce60830a26da64059a69
kobaroh <61962552+kobaroh@users.noreply.github.com>
2022-06-25 13:09:27 +0100

    crud

---
+++ I4G0015293VA/blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def Post_List_View(listview):
+    models = posts
+
+
+def Post_Create_View(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+def Post_Detail_View(DetailView):
+
+
+
+
+
+# Create your views here.
